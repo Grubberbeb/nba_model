ecc2d9da6d18b4cb31755d1681ebf92a4c3b997e
Grubberbeb <grubberbeb@gmail.com>
2018-09-23 18:05:35 -0500

    Initial python package files

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='nba_model',
+    version='0.1',
+    description='Predictive model for spread and totals betting',
+    url='https://github.com/Grubberbeb/nba_model',
+    author='https:://www.reddit.com/user/grubberbeb',
+    author_email='grubberbeb@gmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+)
